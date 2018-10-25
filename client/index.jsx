b802b1c2e0a7630c71112fa869040586065b6885
Joshua Elder <jcelder88@gmail.com>
2018-10-25 12:06:50 -0700

    Create client boilerplate

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-env browser */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './src/components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ client/src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <div>Photo Module</div>
+);
+
+export default App;
