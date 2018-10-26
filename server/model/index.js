267e9f27b84c806ca1b89e0a3ebcf81684fc0223
Joshua Elder <jcelder88@gmail.com>
2018-10-25 18:46:46 -0700

    Define Photo model

---
+++ server/model/index.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const { db } = require('../db');
+
+const Photo = db.define('photo', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  display_index: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  alt_text: {
+    type: Sequelize.STRING,
+  },
+  verified_photo: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  listing_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  image_url: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Photo;
