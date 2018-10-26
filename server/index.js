e70f432dbf6255be54ca7fbf33ab8d3f90e5577f
Joshua Elder <jcelder88@gmail.com>
2018-10-26 15:42:12 -0700

    Fix linting errors

--- server/index.js
+++ server/index.js
@@ -8,5 +8,6 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server listening on localhost:${port}`);
 });
