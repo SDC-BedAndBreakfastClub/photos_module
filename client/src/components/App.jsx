fba27f36c71aed379fc659c29e560c1bac9abbd8
Joshua Elder <jcelder88@gmail.com>
2018-10-29 16:36:37 -0700

    Update photos state on initial mount

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -9,10 +10,27 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    axios.get('/api/rooms/100/images')
+      .then((response) => {
+        console.log(response);
+        return response.data;
+      })
+      .then((data) => {
+        this.setState({
+          photos: data,
+        });
+      })
+      .catch(e => console.error(e));
+  }
+
   render() {
+    const { photos } = this.state;
     return (
-      <div>Photo Module</div>
-    )
+      <div>
+        Photo Module
+      </div>
+    );
   }
 }
 
