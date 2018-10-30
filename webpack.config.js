f122e2ec501cf199e830f6392fa4344035117f95
Joshua Elder <jcelder88@gmail.com>
2018-10-30 08:07:36 -0700

    Enable css modules

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,19 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64:5]',
+            },
+          },
+        ],
+
+      },
     ],
   },
   resolve: {
