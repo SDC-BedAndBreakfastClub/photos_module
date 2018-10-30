0a7a082f8d113984a1e001fd112e9df46cf0ffa4
Joshua Elder <jcelder88@gmail.com>
2018-10-29 17:00:58 -0700

    Add photoIndex prop

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Photo = ({ imageUrl, altText }) => (
-  <img src={imageUrl} alt={altText} />
+const Photo = ({ imageUrl, altText, photoIndex }) => (
+  <img src={imageUrl} alt={altText} data-index={photoIndex} />
 );
 
 Photo.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
+  photoIndex: PropTypes.number.isRequired,
 };
 
 export default Photo;
