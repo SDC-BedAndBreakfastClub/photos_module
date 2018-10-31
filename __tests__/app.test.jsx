8f11d72842b6cef00d86f8cce975ced299046c04
Joshua Elder <jcelder88@gmail.com>
2018-10-31 16:10:22 -0700

    Add test for mouse entering the component

--- __tests__/app.test.jsx
+++ __tests__/app.test.jsx
@@ -2,12 +2,23 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import App from '../client/src/components/App/App';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
 describe('App Component Unit Tests', () => {
+  test('wrapper exists', () => {
+    expect(wrapper.find('.container')).toBeDefined();
+  });
   test('it has the correct initialized state prior to componentDidMount', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.state(['photos'])).toEqual([]);
+  });
+  test('it should toggle the isHeroHovered key in state when the mouse enters the component', () => {
+    wrapper.simulate('mouseEnter');
+    expect(wrapper.state(['isHeroHovered'])).toBe(true);
   });
 });
