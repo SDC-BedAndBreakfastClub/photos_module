bb46d36b09b324b349c7dea9fedea151022f2d80
Joshua Elder <jcelder88@gmail.com>
2018-11-02 10:24:57 -0700

    Add tests

--- __tests__/backdrop.test.jsx
+++ __tests__/backdrop.test.jsx
@@ -2,3 +2,26 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
+
+import Backdrop from '../client/src/components/Modal/Backdrop/Backdrop';
+
+describe('Backdrop Unit Tests', () => {
+  test('The wrapper should exist', () => {
+    const wrapper = shallow(<Backdrop showModal />);
+    expect(wrapper).toBeDefined();
+  });
+  test('it should render when the showModal prop is true', () => {
+    const wrapper = shallow(
+      <Backdrop
+        showModal
+      />,
+    );
+    expect(wrapper.find('div')).toHaveLength(1);
+  });
+  test('it should not render the showModal prop is false', () => {
+    const wrapper = shallow(
+      <Backdrop showModal={false} />,
+    );
+    expect(wrapper.find('div')).toHaveLength(0);
+  });
+});
--- __tests__/modal.test.jsx
+++ __tests__/modal.test.jsx
@@ -2,3 +2,26 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
+
+import Modal from '../client/src/components/Modal/Modal';
+
+describe('Modal Unit Tests', () => {
+  test('The wrapper should exist', () => {
+    const wrapper = shallow(<Modal showModal />);
+    expect(wrapper).toBeDefined();
+  });
+  test('it should render when the showModal prop is true', () => {
+    const wrapper = shallow(
+      <Modal
+        showModal
+      />,
+    );
+    expect(wrapper.find('div')).toHaveLength(1);
+  });
+  test('it should not render the showModal prop is false', () => {
+    const wrapper = shallow(
+      <Modal showModal={false} />,
+    );
+    expect(wrapper.find('div')).toHaveLength(0);
+  });
+});
