e28900b004515b70d5308ad35555f4e572d7d739
Joshua Elder <jcelder88@gmail.com>
2018-11-02 15:32:10 -0700

    Invoke showModalHandler on click

--- __tests__/modalCloseButton.test.jsx
+++ __tests__/modalCloseButton.test.jsx
@@ -2,3 +2,22 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
+import ModalCloseButton from '../client/src/components/Modal/ModalCloseButton/ModalCloseButton';
+
+let showModalHandler;
+let wrapper;
+
+beforeEach(() => {
+  showModalHandler = jest.fn();
+  wrapper = shallow(<ModalCloseButton showModalHandler={showModalHandler} />);
+});
+
+describe('ModalCloseButton Unit Tests', () => {
+  test('the wrapper should exist', () => {
+    expect(wrapper).toBeDefined();
+  });
+  test('it should invoke the showModalHandler when clicked', () => {
+    wrapper.find('button').simulate('click');
+    expect(showModalHandler).toBeCalledTimes(1);
+  });
+});
--- client/src/components/Modal/ModalCloseButton/ModalCloseButton.jsx
+++ client/src/components/Modal/ModalCloseButton/ModalCloseButton.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './ModalCloseButton.css';
 
-const ModalCloseButton = () => (
+const ModalCloseButton = ({ showModalHandler }) => (
   <button
     type="button"
+    onClick={() => showModalHandler()}
+    className={styles.btn}
   >
     <svg viewBox="0 0 24 24" role="img" aria-label="Close" focusable="false" className={styles.closeSvg}>
       <path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fillRule="evenodd" />
     </svg>
   </button>
 );
+
+ModalCloseButton.propTypes = {
+  showModalHandler: PropTypes.func.isRequired,
+};
 
 export default ModalCloseButton;
