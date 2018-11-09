f458f5e38664749d4b6681ecff56d46aec96aace
Joshua Elder <jcelder88@gmail.com>
2018-11-09 13:32:45 -0800

    Initial commit

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'client'),
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64:5]',
+            },
+          },
+        ],
+
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+};
