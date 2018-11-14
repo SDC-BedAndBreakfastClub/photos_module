47651c6c88fcd3a3d8f8276da34148b1f19cfafe
Danny <ngo.danny11@gmail.com>
2018-11-13 22:46:00 -0800

    Making new changes to database

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,1 @@
-const Sequelize = require('sequelize');
-
-const db = new Sequelize('airbnb_clone', process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-  host: process.env.MYSQL_HOST,
-  dialect: 'mysql',
-});
-
-module.exports = db;
+///
