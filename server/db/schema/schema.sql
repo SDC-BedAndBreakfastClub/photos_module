f7bc914b3bd679b2ba8e819484516a15a415a798
Danny <ngo.danny11@gmail.com>
2018-11-16 20:21:14 -0800

    Refactor seed script and schema

--- server/db/schema/schema.sql
+++ server/db/schema/schema.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS airbnb_photos;
 
-USE airbnb_photos;
+\connect airbnb_photos;
 
 DROP TABLE IF EXISTS photos;
 DROP TABLE IF EXISTS listings;
 
 CREATE TABLE listings (
-  listing_id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  listing_id serial PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE photos (
-  id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id serial PRIMARY KEY,
   listing_id INT NOT NULL,
   image_url text NOT NULL,
   description text NOT NULL,
@@ -21,6 +21,7 @@ CREATE TABLE photos (
   foreign key (listing_id) references listings (listing_id)
 );
 
+\copy listings(name) FROM '/Users/dannyngos/HRSF105/SDC/airbnb-clone-photo-module/utils/db/listings.csv' DELIMITER ',' CSV HEADER;
 
-
+\copy photos(listing_id,image_url,description,is_verified_photo,createdAt,updatedAt) FROM '/Users/dannyngos/HRSF105/SDC/airbnb-clone-photo-module/utils/db/photos.csv' DELIMITER ',' CSV HEADER;
 
--- utils/db/seedDB.js
+++ utils/db/seedDB.js
@@ -3,38 +3,37 @@ const path =  require('path');
 const fs = require('fs');
 const moment = require('moment');
 
-let i = 0;
-let j = 0;
 
 const streamListing = fs.createWriteStream(path.join(__dirname, 'listings.csv'));
-const streamPhotos = fs.createWriteStream(path.join(__dirname, 'photos.csv'));
+streamListing.write('name\n');
 
+let i = 0;
 function writeListings() {
-  streamListing.write('listing_id, name\n');
   while (i < 10000000) {
     i++;
-    const listing_id = i;
     const name = faker.commerce.productName();
-    if (!streamListing.write(`${listing_id},${name}\n`)) {
+    if (!streamListing.write(`${name}\n`)) {
       return;
     }
   }
   streamListing.end();
 }
 
+const streamPhotos = fs.createWriteStream(path.join(__dirname, 'photos.csv'));
+streamPhotos.write('listing_id, image_url, description, is_verified_photo, createdAt, updatedAt\n');
+
+let j = 0;
 function writePhotos () {
-  streamPhotos.write('id, listing_id, image_url, description, is_verified_photo, createdAt, updatedAt\n');
   while (j < 100000000) {
     j++;
     const urlPath = 'https://s3-us-west-1.amazonaws.com/hrsdc/';
-    const id = j;
     const listing_id = Math.floor(Math.random() * 10000000) + 1;
     const image_url = urlPath + Math.floor(Math.random() * 33 +1) + '.jpg'; //33 images in S3
-    const description = faker.lorem.text();
+    const description = faker.lorem.words();
     const is_verified_photo = !!Math.floor(Math.random() * 2);
     const createdAt = moment(faker.date.past()).format('YYYY-MM-DD');
     const updatedAt = moment(faker.date.recent()).format('YYYY-MM-DD');
-    if (!streamPhotos.write(`${id},${listing_id},${image_url},${description},${is_verified_photo},${createdAt},${updatedAt}\n`)) {
+    if (!streamPhotos.write(`${listing_id},${image_url},${description},${is_verified_photo},${createdAt},${updatedAt}\n`)) {
       return;
     }
   }
@@ -50,53 +49,3 @@ streamPhotos.on('drain', function () {
   writePhotos();
 });
 writePhotos();
-
-
-// create photos table
-// const photosInfo =[];
-// for (let i = 0; i < 100000; i++) {
-//   let photos = {};
-//   let urlPath = 'https://s3-us-west-1.amazonaws.com/hrsdc/';
-//   photos.id = i + 1;
-//   photos.listing_id = Math.floor(Math.random() * 100000) +1;
-//   photos.image_url = urlPath + Math.floor(Math.random() *33 +1) + '.jpg';
-//   photos.description = faker.lorem.text();
-//   photos.is_verified_photo = !!Math.floor(Math.random() * 2);
-//   photos.createdAt = faker.date.past();
-//   photos.createdAt = moment(photos.createdAt).format('YYYY-MM-DD');
-//   photos.updatedAt = faker.date.recent();
-//   photos.updatedAt = moment(photos.updatedAt).format('YYYY-MM-DD');
-//   photosInfo.push(photos);
-// }
-
-
-
-// const csvConverter = (arr) => {
-//   let output = '';
-//   let column = [];
-//   for (let key in arr[0]) {
-//     column.push(key);
-//   }
-//   output += `${column.join()}\n`;
-//   for (let i = 0; i < arr.length; i++) {
-//     column = [];
-//     for (let key in arr[i]) {
-//       column.push(arr[i][key]);
-//     }
-//     output += `${column.join()}\n`;
-//   }
-//   return output;
-// };
-//
-// const listingsCSV = csvConverter(listingInfo);
-// const photosCSV = csvConverter(photosInfo);
-//
-// fs.createWriteStream(path.join(__dirname, 'listings.csv'), listingsCSV, (err) => {
-//   if (err) throw err;
-//   console.log('saved');
-// });
-//
-// fs.createWriteStream(path.join(__dirname, 'photos.csv'), photosCSV, (err) => {
-//   if (err) throw err;
-//   console.log('saved');
-// });
